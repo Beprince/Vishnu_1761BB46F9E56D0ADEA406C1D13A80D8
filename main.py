6179035da353a3975a6df6e85b1aa14be63acad9
Beprince <144431528+Beprince@users.noreply.github.com>
2023-09-25 14:45:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-def recur_factorial(n):  
-   if n == 1:  
-       return n  
-   else:  
-       return n*recur_factorial(n-1)  
-# take input from the user  
-num = int(input("Enter a number: "))  
-# check is the number is negative  
-if num < 0:  
-   print("Sorry, factorial does not exist for negative numbers")  
-elif num == 0:  
-   print("The factorial of 0 is 1")  
-else:  
-   print("The factorial of",num,"is",recur_factorial(num))  
+class BankAccount:
+
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited {}. New balance: {}".format(amount,
+                                                   self.__account_balance))
+    else:
+      print("Invalid deposit amount. Please deposit a positive amount.")
+
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      # self.__account_balance = self._account balance - amount
+      print("Withdrew {}. New balance: {}".format(amount,
+                                                  self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient halance.")
+
+  def display_balance(self):
+    print("Account balance for {} (Account #{}): {}".format(
+        self.__account_holder_name, self.__account_number,
+        self.__account_balance))
+
+
+# Create an instance of the BankAccount class
+account = BankAccount(account_number="123456789",
+                      account_holder_name="Hari Prabu",
+                      initial_balance=5000.0)
+
+#Test deposit and wthdraw functionally
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.withdraw(20000.0)
+account.display_balance()
